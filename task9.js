52f52b013390708d6e7f4af9954884fce43c98a5
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2021-08-17 15:06:16 +0300

    added new tasks

--- task9.js
+++ task9.js
@@ -1,6 +1,16 @@
 const fileSystem = require('fs');
 
-fileSystem.truncate('files/readme.txt', err => {
-    if(err) throw err;
-    console.log('cleaning');
-})
+// fileSystem.truncate('files/readme.txt', 1, err => {
+//     if(err) throw err;
+//     console.log('cleaning');
+// })
+
+let interval = setInterval(() => {
+    for (let index = 1; index <= 100; index++) {
+        if(index === 100) clearInterval(interval)
+        fileSystem.writeFile(`./files/readme.txt`, `${index}`, (err) => {
+            if(err) throw err;
+        });
+        console.log(`${index} was writing`);
+    }
+}, 1000);
